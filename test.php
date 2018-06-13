46c7907b63f4bbb4ad0002d36c51fc46e121b5b1
bonnie871017 <39939571+bonnie871017@users.noreply.github.com>
2018-06-13 08:34:52 +0800

    商品內頁(新增留言板)

--- test.php
+++ test.php
@@ -317,9 +317,21 @@ session_start();
             <!--boxify-container-->
           </div>
         </div>
+        <div class="fb-comments" data-href="test.php" data-numposts="5" data-width=100%></div>
       </div>
     </section>
     <!-- END section -->
+    <div id="fb-root"></div>
+    <script>
+      (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = 'https://connect.facebook.net/zh_TW/sdk.js#xfbml=1&version=v3.0';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+    </script>
     <footer class="ftco-footer ftco-bg-dark ftco-section">
       <div class="container">
         <div class="row mb-5">
