84c47cf9496d230bef15f74da1a4a33d9ffec9ae
bonnie871017 <39939571+bonnie871017@users.noreply.github.com>
2018-06-18 03:18:41 +0800

    連不到資料庫的購物車內頁

---
+++ js/cart_web.js
@@ -0,0 +1,47 @@
+$(function() { //網頁完成後才會載入
+
+        $.ajax({
+            url: "cart_web_products_ajax.php",
+            dataType: 'text',
+            success: function(msg) {
+                $("#CRUD").html(msg); //顯示訊息
+                //document.getElementById('show_msg').innerHTML= msg ;
+            },
+            error: function(xhr, ajaxOptions, thrownError) {
+                alert(xhr.status);
+                alert(thrownError);
+            }
+        });
+        $.ajax({
+            url: "js/cart_web_products.js",
+            dataType: "script",
+            success: function() {},
+            error: function(xhr, ajaxOptions, thrownError) {
+                alert(xhr.status);
+                alert(thrownError);
+            }
+        });
+    $('#products_select').click(function() {
+        $.ajax({
+            url: "cart_web_products_ajax.php",
+            dataType: 'text',
+            success: function(msg) {
+                $("#CRUD").html(msg); //顯示訊息
+                //document.getElementById('show_msg').innerHTML= msg ;
+            },
+            error: function(xhr, ajaxOptions, thrownError) {
+                alert(xhr.status);
+                alert(thrownError);
+            }
+        });
+        $.ajax({
+            url: "js/cart_web_products.js",
+            dataType: "script",
+            success: function() {},
+            error: function(xhr, ajaxOptions, thrownError) {
+                alert(xhr.status);
+                alert(thrownError);
+            }
+        });
+    });
+});
