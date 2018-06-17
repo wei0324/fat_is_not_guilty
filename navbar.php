123660306c7c8bcc9e79523180ae9be4c0aa839d
wei0324 <39876589+wei0324@users.noreply.github.com>
2018-06-17 23:19:21 +0800

    navbar不會長高了喔~

--- navbar.php
+++ navbar.php
@@ -1,4 +1,3 @@
-
 <?php
   include("passwordEdit.php"); 
     if (isset($_SESSION['cart'])) {
@@ -7,11 +6,44 @@
         $cnt = 0;
     }
 ?>
-<div id="gotop"><i class="fas fa-angle-up"></i></div>
-<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
-    <div class="container">
-        <a class="navbar-brand" href="index.php">胖不是罪</a>
-        <div id="cart1">
+    <div id="gotop"><i class="fas fa-angle-up"></i></div>
+    <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+        <div class="container">
+            <a class="navbar-brand" href="index.php">胖不是罪</a>
+            <div id="cart1">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a href="" class="nav-link">
+                            <div class="form-inline">
+                                <div style="position: relative; width: 35px; height: 35px;">
+                                    <div style="position: absolute;left: 0px;bottom: 0;">
+                                        <i class="fas fa-shopping-cart" style="font-size: 25px"></i>
+                                    </div>
+                                    <div id="cart" style="position: absolute;right: 0;top: 0;">
+                                        <?php echo $cnt; ?>
+                                    </div>
+                                </div>
+                                購物車
+                            </div>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="oi oi-menu"></span> Menu
+            </button>
+            <div class="collapse navbar-collapse" id="ftco-nav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active"><a href="index.php#section-home" class="nav-link">首頁</a></li>
+                    <li class="nav-item"><a href="index.php#section-offer" class="nav-link">本日精選</a></li>
+                    <li class="nav-item"><a href="index.php#section-menu" class="nav-link">商品專區</a></li>
+                    <li class="nav-item"><a href="index.php#section-news" class="nav-link">活動快訊</a></li>
+                    <li class="nav-item"><a href="index.php#section-about" class="nav-link">關於我們</a></li>
+                    <li class="nav-item"><a href="index.php#section-contact" class="nav-link">連繫我們</a></li>
+                </ul>
+            </div>
+        </div>
+        <div id="cart2">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
                     <a href="" class="nav-link">
@@ -20,7 +52,9 @@
                                 <div style="position: absolute;left: 0px;bottom: 0;">
                                     <i class="fas fa-shopping-cart" style="font-size: 25px"></i>
                                 </div>
-                                <div id="cart" style="position: absolute;right: 0;top: 0;"><?php echo $cnt; ?></div>
+                                <div id="cart" style="position: absolute;right: 0;top: 0;">
+                                    <?php echo $cnt; ?>
+                                </div>
                             </div>
                             購物車
                         </div>
@@ -28,94 +62,62 @@
                 </li>
             </ul>
         </div>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="oi oi-menu"></span> Menu
-        </button>
-        <div class="collapse navbar-collapse" id="ftco-nav">
+        <?php if (isset($_SESSION['account'])): ?>
+        <div>
+            <!-- member -->
             <ul class="navbar-nav ml-auto">
-                <li class="nav-item active"><a href="index.php#section-home" class="nav-link">首頁</a></li>
-                <li class="nav-item"><a href="index.php#section-offer" class="nav-link">本日精選</a></li>
-                <li class="nav-item"><a href="index.php#section-menu" class="nav-link">商品專區</a></li>
-                <li class="nav-item"><a href="index.php#section-news" class="nav-link">活動快訊</a></li>
-                <li class="nav-item"><a href="index.php#section-about" class="nav-link">關於我們</a></li>
-                <li class="nav-item"><a href="index.php#section-contact" class="nav-link">連繫我們</a></li>
-            </ul>
-        </div>
-    </div>
-    <div id="cart2">
-        <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-                <a href="" class="nav-link">
+                <li class="nav-item">
+                  <a href="" target="_blank" class="nav-link" data-toggle="modal" data-target="#Edit">
                     <div class="form-inline">
                         <div style="position: relative; width: 35px; height: 35px;">
                             <div style="position: absolute;left: 0px;bottom: 0;">
-                                <i class="fas fa-shopping-cart" style="font-size: 25px"></i>
+                                
+                                <i class="fas fa-user-circle"></i>
                             </div>
-                            <div id="cart" style="position: absolute;right: 0;top: 0;"><?php echo $cnt; ?></div>
+                            <div style="position: absolute;right: 0;top: 0;"></div>
                         </div>
-                        購物車
+                        <?php echo $_SESSION['account']; ?>
                     </div>
-                </a>
-            </li>
-        </ul>
-    </div>
-    <?php if (isset($_SESSION['account'])): ?>
-      <div> <!-- member -->
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item">
-            <a href="" class="nav-link">
-              <div class="form-inline">
-                <div style="position: relative; width: 35px; height: 35px;">
-                  <div style="position: absolute;left: 0px;bottom: 0;">
-                    <a href="" target="_blank" class="nav-link" data-toggle="modal" data-target="#Edit">
-                    <i class="fas fa-user-circle"></i>
-                  </div>
-                  <div  style="position: absolute;right: 0;top: 0;"></div>
-                </div>
-                <?php echo $_SESSION['account']; ?>
-              </div>
-            </a>
-          </li>
-        </ul>
-      </div>
-
-      <div> <!-- Logout -->
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item">
-            <a href="logout.php" class="nav-link" id="logout-1" >
-              <div class="form-inline">
-                <div style="position: relative; width: 35px; height: 35px;">
-                  <div style="position: absolute;left: 0px;bottom: 0;">
-                    <i class="fas fa-power-off"></i>
-                  </div>
-                  <div  style="position: absolute;right: 0;top: 0;"></div>
-                </div>
-                登出
-              </div>
-            </a>
-          </li>
-        </ul>
-      </div>
-      <?php else: ?>
-        <div> <!-- Logout -->
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-              <a href="login.php" class="nav-link" id="login-1" >
-                <div class="form-inline">
-                  <div style="position: relative; width: 35px; height: 35px;">
-                    <div style="position: absolute;left: 0px;bottom: 0;">
-                      <i class="fas fa-power-off"></i>
-                    </div>
-                    <div  style="position: absolute;right: 0;top: 0;"></div>
-                  </div>
-                  登入
-                </div>
-              </a>
-            </li>
-          </ul>
+                    </a>
+                </li>
+            </ul>
         </div>
-    <?php endif; ?>
-
-
-
-</nav>
+        <div>
+            <!-- Logout -->
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a href="logout.php" class="nav-link" id="logout-1">
+                        <div class="form-inline">
+                            <div style="position: relative; width: 35px; height: 35px;">
+                                <div style="position: absolute;left: 0px;bottom: 0;">
+                                    <i class="fas fa-power-off"></i>
+                                </div>
+                                <div style="position: absolute;right: 0;top: 0;"></div>
+                            </div>
+                            登出
+                        </div>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <?php else: ?>
+        <div>
+            <!-- Logout -->
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a href="login.php" class="nav-link" id="login-1">
+                        <div class="form-inline">
+                            <div style="position: relative; width: 35px; height: 35px;">
+                                <div style="position: absolute;left: 0px;bottom: 0;">
+                                    <i class="fas fa-power-off"></i>
+                                </div>
+                                <div style="position: absolute;right: 0;top: 0;"></div>
+                            </div>
+                            登入
+                        </div>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <?php endif; ?>
+    </nav>
