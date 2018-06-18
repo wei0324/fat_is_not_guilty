94f82d31b493ce4c35814dbd4126d0d95ca91473
mg214 <zxc880214@gmail.com>
2018-06-18 23:33:52 +0800

    include loader

--- index.php
+++ index.php
@@ -481,41 +481,9 @@ mysqli_close($link); // 關閉資料庫連結
             </div>
         </div>
     </footer>
-      <?php include("login&sign_up(modal.php"); ?>
-    <!-- loader -->
-    <div id="ftco-loader" class="show fullscreen">
-        <svg class="circular" width="48px" height="48px">
-            <circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee" />
-            <circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00" />
-        </svg>
-    </div>
-    <script src="js/jquery.min.js"></script>
-    <script src="js/popper.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/jquery.easing.1.3.js"></script>
-    <script src="js/jquery.waypoints.min.js"></script>
-    <script src="js/owl.carousel.min.js"></script>
-    <script src="js/jquery.magnific-popup.min.js"></script>
-    <script src="js/bootstrap-datepicker.js"></script>
-    <script src="js/jquery.timepicker.min.js"></script>
-    <script src="js/jquery.animateNumber.min.js"></script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
-    <script src="js/google-map.js"></script>
-    <script src="js/main.js"></script>
-    <script type="text/javascript">
-    $("#gotop").click(function() {
-        jQuery("html,body").animate({
-            scrollTop: 0
-        }, 500);
-    });
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 300) {
-            $('#gotop').fadeIn("fast");
-        } else {
-            $('#gotop').stop().fadeOut("fast");
-        }
-    });
-    </script>
+      <?php include("login&sign_up(modal.php");
+      include("loader.php");?>
+
 </body>
 
 </html>
